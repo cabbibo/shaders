b16d2234bc98dbd6cc60bce43c09cb0e918a874e
Isaac Cohen <icohen@leapmotion.com>
2014-04-26 11:14:50 -0700

    making changes that break the signDistance example. Than checking those changes in, along with changes to every single other file. YOLO

--- utils/TextParticles.js
+++ utils/TextParticles.js
@@ -3,9 +3,6 @@
 // TODO: Later Version, create texture to place particles 
 // If you are using your own texture, 
 // make sure that you are using your own letter indicies
-var letterIndicies = {A:[1,2],B:[2,2],C:[3,2],D:[4,2],E:[5,2],F:[6,2],G:[7,2],H:[8,2],I:[9,2],J:[10,2],K:[11,2],L:[12,2],M:[13,2],N:[14,2],O:[15,2],P:[0,3],Q:[1,3],R:[2,3],S:[3,3],T:[4,3],U:[5,3],V:[6,3],W:[7,3],X:[8,3],Y:[9,3],Z:[10,3],a:[1,4],b:[2,4],c:[3,4],d:[4,4],e:[5,4],f:[6,4],g:[7,4],h:[8,4],i:[9,4],j:[10,4],k:[11,4],l:[12,4],m:[13,4],n:[14,4],o:[15,4],p:[0,5],q:[1,5],r:[2,5],s:[3,5],t:[4,5],u:[5,5],v:[6,5],w:[7,5],x:[8,5],y:[9,5],z:[10,5],"!":[1,0],'"':[2,0],'#':[3,0],'$':[4,0],'%':[5,0],'&':[6,0],"'":[7,0],"(":[8,0],")":[9,0],"*":[10,0],"+":[11,0],",":[12,0],"-":[13,0],".":[14,0],"/":[15,0],"0":[0,1],"1":[1,1],"2":[2,1],"3":[3,1],"4":[4,1],"5":[5,1],"6":[6,1],"7":[7,1],"8":[8,1],"9":[9,1],":":[10,1],";":[11,1],"<":[12,1],"=":[13,1],">":[14,1],"?":[15,1],"@":[0,2],"[":[11,3],"]":[13,3],"^":[14,3],"_":[15,3],"{":[11,5],"|":[12,5],"}":[13,5],"~":[14,5],}
- 
-  
   
   function TextParticles( params ){ 
    
@@ -28,11 +25,28 @@ var letterIndicies = {A:[1,2],B:[2,2],C:[3,2],D:[4,2],E:[5,2],F:[6,2],G:[7,2],H:
 
   }
 
+  TextParticles.prototype.createDebugMesh = function( texture , size ){
+
+    var size = size || 50;
+    var geo = new THREE.PlaneGeometry( size , size );
+    var mat = new THREE.MeshBasicMaterial({
+      map: texture
+    });
+
+    var mesh = new THREE.Mesh( geo , mat );
+
+    return mesh;
+
+
+  }
   TextParticles.prototype.createTextParticles = function( string, params ){
 
+    this.lookupTexture = lookup;
+    
     var particles = this.createParticles( string );
-
+    var lookup    = this.createLookupTexture( particles );
     var geometry  = this.createGeometry( particles );
+   
     var material  = this.createMaterial( params );
 
     var particleSystem = new THREE.ParticleSystem( geometry , material );
@@ -89,9 +103,44 @@ var letterIndicies = {A:[1,2],B:[2,2],C:[3,2],D:[4,2],E:[5,2],F:[6,2],G:[7,2],H:
     return particles;
 
   }
- 
-  // Should it be centered?
-  TextParticles.prototype.createGeometry = function( particles ){
+
+  
+
+  TextParticles.prototype.createLookupTexture = function( particles ){
+
+    var size = Math.ceil( Math.sqrt( particles.length ));
+    var data = new Float32Array( size * size * 4 );
+
+    for( var i = 0; i < size*size; i++ ){
+
+      if( particles[i] ){
+      
+        tc = this.getTextCoordinates( particles[i][0] );
+
+        data[ i * 4 + 0 ] = particles[i][1] * this.letterWidth / 50000000;
+        data[ i * 4 + 1 ] = particles[i][2] * this.lineHeight / 5000000;
+        data[ i * 4 + 2 ] = tc[0]; // packing in textCoord 
+        data[ i * 4 + 3 ] = tc[1]; // just cuz!
+
+      }
+
+    }
+
+    var f = THREE.RGBAFormat;
+    var t = THREE.FloatType;
+    
+    var texture = new THREE.DataTexture( data , size, size, f , t );
+   
+    texture.minFilter = THREE.NearestFilter;
+    texture.magFilter = THREE.NearestFilter;
+    texture.generateMipmaps = false;
+    texture.needsUpdate = true;
+
+    return texture;
+  
+  }
+
+  TextParticles.prototype.createGeometry = function( particles , lookup ){
 
     var geometry = new THREE.BufferGeometry();
     
@@ -101,12 +150,28 @@ var letterIndicies = {A:[1,2],B:[2,2],C:[3,2],D:[4,2],E:[5,2],F:[6,2],G:[7,2],H:
     var positions   = geometry.attributes.position.array;
   	var textCoords  = geometry.attributes.textCoord.array;
 
+    var lookupWidth = Math.ceil( Math.sqrt( particles.length ) ); 
+
+
     for( var i = 0; i < particles.length; i++ ){
     
+      if( lookup ){
 
-      positions[ i * 3 + 0 ] = particles[i][1] * this.letterWidth;
-      positions[ i * 3 + 1 ] = -particles[i][2] * this.lineHeight; 
-      positions[ i * 3 + 2 ] = 0; 
+        var x =             i % lookupWidth   ;
+        var y = Math.floor( i / lookupWidth ) ;
+
+        positions[ i * 3 + 0 ] = x;
+        positions[ i * 3 + 1 ] = y; 
+        positions[ i * 3 + 2 ] = 0; 
+
+      }else{
+
+        positions[ i * 3 + 0 ] = particles[i][1] * this.letterWidth;
+        positions[ i * 3 + 1 ] = -particles[i][2] * this.lineHeight; 
+        positions[ i * 3 + 2 ] = 0; 
+
+      }
+        
 
       tc = this.getTextCoordinates( particles[i][0] );
 
@@ -145,38 +210,50 @@ var letterIndicies = {A:[1,2],B:[2,2],C:[3,2],D:[4,2],E:[5,2],F:[6,2],G:[7,2],H:
 
   }
 
-  
+ 
+  // Passing a lookup through
   TextParticles.prototype.createMaterial = function( params ){
 
     var params = params || {};
 
-    var color = params.color          || this.color;
-    var vert  = params.vertexShader   || this.vertexShader;
-    var frag  = params.fragmentShader || this.fragmentShader;
+    var texture = params.texture  || this.texture;
+    var lookup  = params.lookup   || this.lookupTexture;
+    var color   = params.color    || this.color;
+
 
     var attributes = {
-      textCoord: { type:"v4" , value: null }
+      textCoord: { type:"v4" , value: null },
     }
 
-    uniforms = {
+   
+    var c = new THREE.Color( color );
+    var uniforms = {
 
-      t_text:{ type:"t" , value: this.texture },
-      color:{ type:"c" , value:new THREE.Color( color ) },
+      t_lookup:{  type:"t" , value: lookup  },
+      t_text:{    type:"t" , value: texture },
+      color:{     type:"c" , value: c       }
   
     }
-        
-    var vert = params.vertShader || this.vertexShader;
-    var frag = params.fragShader || this.fragmentShader;
+
+    var vert  = params.vertexShader   || this.vertexShader;
+    var frag  = params.fragmentShader || this.fragmentShader;
+    var attr  = params.attributes     || attributes;
+    var unif  = params.uniforms       || uniforms;
+
+    var blend = params.blending       || THREE.AdditiveBlending;
+    var depth = params.depthWrite     || false;
+    var trans = params.transparent    || true;
 
     material = new THREE.ShaderMaterial({
 
-      uniforms:uniforms,
-      attributes: attributes,
-      vertexShader: vert,
-      fragmentShader: frag,
-      transparent: true,
-      depthWrite: false,
-      blending:THREE.AdditiveBlending
+      uniforms:           unif,
+      attributes:         attr,
+      vertexShader:       vert,
+      fragmentShader:     frag,
+
+      transparent:        trans,
+      depthWrite:         depth,
+      blending:           blend
 
     });
 
@@ -185,7 +262,28 @@ var letterIndicies = {A:[1,2],B:[2,2],C:[3,2],D:[4,2],E:[5,2],F:[6,2],G:[7,2],H:
 
 
   }
- 
+
+
+
+  var letterIndicies = {
+    A:[1,2],B:[2,2],C:[3,2],D:[4,2],E:[5,2],F:[6,2],G:[7,2],
+    H:[8,2],I:[9,2],J:[10,2],K:[11,2],L:[12,2],M:[13,2],
+    N:[14,2],O:[15,2],P:[0,3],Q:[1,3],R:[2,3],S:[3,3],
+    T:[4,3],U:[5,3],V:[6,3],W:[7,3],X:[8,3],Y:[9,3],
+    Z:[10,3],a:[1,4],b:[2,4],c:[3,4],d:[4,4],e:[5,4],
+    f:[6,4],g:[7,4],h:[8,4],i:[9,4],j:[10,4],k:[11,4],
+    l:[12,4],m:[13,4],n:[14,4],o:[15,4],p:[0,5],q:[1,5],
+    r:[2,5],s:[3,5],t:[4,5],u:[5,5],v:[6,5],w:[7,5],x:[8,5],
+    y:[9,5],z:[10,5],"!":[1,0],'"':[2,0],'#':[3,0],
+    '$':[4,0],'%':[5,0],'&':[6,0],"'":[7,0],"(":[8,0],
+    ")":[9,0],"*":[10,0],"+":[11,0],",":[12,0],"-":[13,0],
+    ".":[14,0],"/":[15,0],"0":[0,1],"1":[1,1],"2":[2,1],
+    "3":[3,1],"4":[4,1],"5":[5,1],"6":[6,1],"7":[7,1],
+    "8":[8,1],"9":[9,1],":":[10,1],";":[11,1],"<":[12,1],
+    "=":[13,1],">":[14,1],"?":[15,1],"@":[0,2],"[":[11,3],
+    "]":[13,3],"^":[14,3],"_":[15,3],"{":[11,5],"|":[12,5],
+    "}":[13,5],"~":[14,5],
+  }
 
 
 
